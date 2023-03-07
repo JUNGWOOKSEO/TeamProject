f4211c5d16b43c24fce321e6f59b7a64e29de080
JUNGWOOK SEO <wjddnr92@naver.com>
2023-03-07 09:20:09 +0900

    il

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MyAjax from "./MyAjax";
+function App() {
+  return (
+    <div className="App">
+      Ajax test;
+      <br/>
+      <MyAjax/>
+    </div>
+  );
+}
+
+export default App;
